57b981f0dc0f73cbc4f3ea3fca0d771797b603d5
Wytze <wytzebusiness@gmail.com>
2026-07-19 16:30:16 +0200

    Release Gyro v0.1.0-alpha.28

--- packaging/homebrew/Formula/gyro.rb
+++ packaging/homebrew/Formula/gyro.rb
@@ -1,7 +1,7 @@
 class Gyro < Formula
   desc "Open-source local-first coding agent workspace CLI"
   homepage "https://github.com/wytzeh197/Gyro"
-  version "0.1.0-alpha.27"
+  version "0.1.0-alpha.28"
   license "Apache-2.0"
 
   # The tagged release workflow generates gyro.rb with the real immutable
